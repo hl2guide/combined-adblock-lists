57b509e7a399f117a125c8f328799044fe1e8728
Dean <hl2guide@gmail.com>
2026-09-23 22:24:34 +1000

    new

--- create_cosmetic_list_v2.py
+++ create_cosmetic_list_v2.py
@@ -256,6 +256,12 @@ TESTING_URLS = [
     f"{URL_PREFIX_GH}/{URL_PART_ADG}/{URL_PART_2}/filter.txt",
     # AdGuard Widgets filter
     f"{URL_PREFIX_GH}/{URL_PART_ADG}/filter_22_Annoyances_Widgets/filter.txt",
+    # TESTING AdGuard
+    # Base filter
+    f"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
+    # Tracking
+    f"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt",
+    f"https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_17_TrackParam/filter.txt",
     # Adblock Warning Removal List
     f"{URL_PREFIX_GH}/{URL_PART_EL}/antiadblock_english.txt",
     # EasyList
